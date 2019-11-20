cc1b9f0583cece233c72b2d18a58c8d53a1e4c8a
DitchDoctors <50470266+DitchDoctors@users.noreply.github.com>
2019-11-20 14:58:56 -0600

    Update help.js

--- commands/nsfw/help.js
+++ commands/nsfw/help.js
@@ -11,19 +11,19 @@ exports.run = async (client, message, args, level) => {
                     "hug gifs and pics ^^ Try tagging a friend ;)")
                 .addField("kiss",
                     "kiss someone or just see a pic/gif")
-                    .addField("doggo",
+                .addField("doggo",
                     "Get a random doggo or pupper pic")
-                    .addField("kitty",
+                .addField("kitty",
                     "Get a random cat or kitten pic")
-                                        .addField("chucknorris",
+                .addField("chucknorris",
                     "everyone likes a good joke, eh?")
-                                        .addField("quote",
+                .addField("quote",
                     "Get a random famous quote")
-                                                            .addField("dadjoke",
+                .addField("dadjoke",
                     "Dad jokes are still a thing, right?")
-                    .addField("kanye",
+                .addField("kanye",
                     "Get a random but real Kanye West quote (potential trigger warnin)")
-                    .addField("trump",
+                .addField("trump",
                     "Get a random but real Donald Trump quote (potential trigger warnin)")
                 .addField("cuddle",
                     "cuddles!")
